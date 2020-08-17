61b64c112a30eda6562f69468f3c354ade92120b
Snake <1025687605@qq.com>
2020-08-17 10:42:51 +0800

    feat: add use-subscrbe (#1)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mut
 
-A tiny state management library based on Immer and useMutableSource
+A tiny state management library based on Immer and useMutableSource or useSubscribe
 
 ## Usage
 
@@ -10,13 +10,7 @@ A tiny state management library based on Immer and useMutableSource
 
 _Note:_
 
-mut require react@experimental and react-dom@experimental
-
-you can
-
-```shell
-yarn add react@experimental react-dom@experimental -D
-```
+if useMutableSource is available then use useMutableSource otherwise use useSubscribe
 
 ### Example
 
---
+++ src/provider.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import { Store } from './store'
+import { MutableSourceContext } from './context'
+
+const { useMemo } = React
+
+interface ProviderProps<T> {
+  store: Store<T>
+  children?: React.ReactNode
+}
+
+function getStoreVersion<T>(store: Store<T>): T {
+  // Because the state is immutable,
+  // it can be used as the "version".
+  return store.getState()
+}
+
+// https://codesandbox.io/s/react-redux-usemutablesource-q18w8?file=/src/fake-react-redux.js:560-1265
+// This mimics the current Redux <Provider> API.
+// It shares the store (really now a MutableSource wrapper)
+// with components below in the tree that read from the store.
+export function Provider<T>({ children, store }: ProviderProps<T>) {
+  const mutableSource = useMemo(() => {
+    // Wrap the Redux store in a MutableSource object.
+    // The useMutableSource() hook works with this type of object.
+    // if can use createMutableSource then use createMutableSource
+    if ((React as any).unstable_createMutableSource) {
+      return (React as any).unstable_createMutableSource(store, getStoreVersion)
+    } else {
+      return store
+    }
+  }, [store])
+
+  return (
+    <MutableSourceContext.Provider value={mutableSource}>{children}</MutableSourceContext.Provider>
+  )
+}
